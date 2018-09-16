056f65eabd8938a04b1c6b2d2fc7c3302bca6f19
KMimura <40707315+KMimura@users.noreply.github.com>
2018-09-16 09:18:44 +0900

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pygame
 import sys
 import random 
@@ -12,25 +14,34 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480)) 
 pygame.display.set_caption("Jump the Rope") 
 heart_image = pygame.image.load("images/heart.png")
-# heart_image = pygame.transform.scale(heart_rough_image, (16,14))
 clock = pygame.time.Clock()
 
 class Octo_Cat:
     def __init__(self,x,y):
+        #position
         self.x = x
         self.y = y
+        #image before scaling
         self.rough_image = pygame.image.load("images/greenoctocat.png").convert()
+        #properly scaled image
         self.image = pygame.transform.scale(self.rough_image, (20,20))
+        #image to show when the player is jumping
         self.immune_image = pygame.transform.scale(self.rough_image, (30,30))
+        #about movements
         self.move_right = False
         self.move_left = False
         self.move_up = False
         self.move_down = False
+        #if jumping or not
         self.immunity = False
+        #the player cannot jump more than a specified amount of time
         self.immunity_count = 0
+        #life
         self.life = 4
+        #if the player is inflicted with damage, it cannot be inflicted again for a certain amount of time
         self.life_lost_time = 0
     
+    #movements
     def update(self,event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
@@ -53,7 +64,7 @@ class Octo_Cat:
             if event.key == pygame.K_DOWN:
                 self.move_down = False
 
-
+#defiens ropes and dots
 class Rope:
     def __init__(self,x=0,y=0,velocity=0,tilt=0,color=0):
         self.x = x
@@ -66,6 +77,7 @@ class Rope:
     def judge(self,octo_cat):
         return
 
+#vertical ropes
 class Straight_Rope(Rope):
     def update(self):
         if(self.x > 635):
@@ -78,12 +90,14 @@ class Straight_Rope(Rope):
             self.x -= self.velocity
         pygame.draw.line(screen, COLORS[3], [self.x, 0], [self.x, 480], 5)
 
+    #checks if the player and the rope collided
     def judge(self,octo_cat):
         if(self.x > (octo_cat.x) and self.x < (octo_cat.x + 20)):
             return True
         else:
             return False
 
+#horizontal ropes
 class Straight_Rope_Horizontal(Rope):
     def update(self):
         if(self.y > 475):
@@ -96,18 +110,21 @@ class Straight_Rope_Horizontal(Rope):
             self.y += self.velocity
         pygame.draw.line(screen, COLORS[3],[0, self.y], [640, self.y], 5)
 
+    #checks if the player and the rope collided
     def judge(self,octo_cat):
         if(self.y > (octo_cat.y) and self.y < (octo_cat.y + 20)):
             return True
         else:
             return False
 
+#dots
 class Shooting_Star(Rope):
     def update(self):
         self.x += self.tilt
         self.y += self.velocity
         pygame.draw.circle(screen, COLORS[self.color], [self.x, self.y], 6)
     
+    #checks if the player and the dot collided
     def judge(self,octo_cat):
         if((self.y > (octo_cat.y) and self.y < (octo_cat.y + 20)) and (self.x > (octo_cat.x) and self.x < (octo_cat.x + 20))):
             return True
@@ -131,6 +148,7 @@ def main():
             else:
                 octo_cat.update(event)
 
+        #move the player
         if octo_cat.move_right == True:
             if octo_cat.x < 620:
                 octo_cat.x += OCTO_CAT_VELOCITY
@@ -144,6 +162,7 @@ def main():
             if octo_cat.y < 460:
                 octo_cat.y += OCTO_CAT_VELOCITY
 
+        #make the instances of ropes and dots
         if (time_elapsed == 20):
             straight_rope = Straight_Rope(0,0,3)
             ropes.append(straight_rope)
@@ -156,7 +175,7 @@ def main():
         if (time_elapsed == 860):
             straight_rope_horizontal = Straight_Rope_Horizontal(0,0,3)
             ropes.append(straight_rope_horizontal)
-        if(random.randrange(500) < 6):
+        if(random.randrange(200) < 6):
             shooting_star1 = Shooting_Star(random.randrange(640),0,random.randrange(5) + 5,random.randrange(10) - 5)
             ropes.append(shooting_star1)
             shooting_star2 = Shooting_Star(random.randrange(640),0,random.randrange(5) + 5,random.randrange(10) - 5)
@@ -166,6 +185,7 @@ def main():
             shooting_star4 = Shooting_Star(10,random.randrange(480),random.randrange(5) + 5,random.randrange(10) - 5)
             ropes.append(shooting_star4)
 
+        #move all the ropes and dots
         for rope in ropes:
             rope.update()
             if (rope.x < 0 or rope.x > 640) or (rope.y < 0 or rope.y > 480):
@@ -173,6 +193,7 @@ def main():
             if(time_elapsed % 1000 == 0) and time_elapsed != 0:
                 rope.velocity += 1
 
+        #if the player is jumping, do not check if it collided with ropes or dots
         if(octo_cat.immunity == True):
             octo_cat.immunity_count += 1
             if (octo_cat.immunity_count < OCTO_CAT_JUMP ):
@@ -194,8 +215,10 @@ def main():
         pygame.display.update()
     quit(time_elapsed) 
 
+#when quitting the game
 def quit(score):
     print("your score: " + str(score))
+    #retrieve the highest score
     data = np.loadtxt("score/score.tsv",dtype="string",delimiter=",")
     highest_score = data[1]
     print("highest score so far: " + highest_score)
